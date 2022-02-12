568280d48ddbdc7a1fcffc3c9b08a1601f5143ac
Hiroki Senoo <hunuaaaa@gmail.com>
2022-02-12 13:56:09 +0900

    設定ファイル更新

--- app/routes/web.php
+++ app/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\TopController@index');
+Route::get('/farm/list', 'App\Http\Controllers\FarmController@index');
+Route::get('/review/list', 'App\Http\Controllers\ReviewController@index');
+Route::get('/content/list', 'App\Http\Controllers\ContentController@index');
+Route::get('/tip/list', 'App\Http\Controllers\TipController@index');
+Route::get('/mypage', 'App\Http\Controllers\MypageController@index');
+Route::get('/register', [App\Http\Controllers\RegisterController::class, 'create'])->middleware('guest')->name('register');
+Route::post('/register', [App\Http\Controllers\RegisterController::class, 'store'])->middleware('guest');
+Route::get('/login', [App\Http\Controllers\LoginController::class, 'index'])->middleware('guest')->name('login');
+Route::post('/login', [App\Http\Controllers\LoginController::class, 'authenticate'])->middleware('guest');
+Route::get('/logout', [App\Http\Controllers\LoginController::class, 'logout'])->middleware('auth')->name('logout');
