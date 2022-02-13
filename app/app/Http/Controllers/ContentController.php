19bf57cdd059954608c21561b16fc166e574d71f
Hiroki Senoo <hunuaaaa@gmail.com>
2022-02-14 00:46:37 +0900

    タイポ修正

--- app/app/Http/Controllers/ContentController.php
+++ app/app/Http/Controllers/ContentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ContentCotroller extends Controller
+class ContentController extends Controller
 {
     public function index() {
         return view('Content.list');
