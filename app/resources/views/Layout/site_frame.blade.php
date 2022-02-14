83fb4494b7d6de8ae4dff1426ba795f63db172e0
Hiroki Senoo <hunuaaaa@gmail.com>
2022-02-14 20:15:52 +0900

    Vue、Tailwindcssを使用したヘッダーの実装

--- app/resources/views/Layout/site_frame.blade.php
+++ app/resources/views/Layout/site_frame.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <title>Top Page</title>
 </head>
 <body>
--- app/resources/views/header.blade.php
+++ app/resources/views/header.blade.php
@@ -1,48 +1,42 @@
-<header>
-    <div class="header-block">
-        <div class="header-left"></div>
-        <div class="header-centre">
-            <figure class="header-logo">
-                <a href="{{ url('/') }}">
-                    <img src="" alt="">
-                </a>
-            </figure>
-            <div class="navigation-top">
-                <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="Top Menu">
-                    <div class="menu-main-container">
-                        <ul id="top-menu" class="menu">
-                            <li class="menu-item">
-                                <a href="{{ url('/farm/list') }}">ファーム</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="{{ url('/review/list') }}">レビュー</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="{{ url('/tip/list') }}">Tip</a>
-                            </li>
-                            <li class="menu-item">
-                                <a href="{{ url('/content/list') }}">コンテンツ</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
+<div id ="app">
+<example-component></example-component>
+</div>
+<div id="header" class="bg-gray-400">
+<header class="container mx-auto text-white">
+    <div class="flex justify-between items-center">
+        <div class="text-4xl font-semibold">Farm Review Site
         </div>
-        <div class="header-right">
-            <div class="header-user">
-                @if (Auth::check())
-                <p><a href="/logout">ログアウト</a></p>
-                @else
-                <a href="{{ url('/register') }}">
-                    <div class="icon icon-off"></div>
-                    <span class="sp-none">新規会員登録</span>
-                </a>
-                <a href="{{ url('/login') }}">
-                    <div class="icon icon-off"></div>
-                    <span class="sp-none">ログイン</span>
-                </a>
-                @endif
-            </div>
+        <div class="md:hidden">
+            <button @click="isOpen = !isOpen" class="focus:outline-none">
+                <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                    <path v-show="!isOpen" d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
+                    <path v-show="isOpen" d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/>                </svg>
+            </button>
         </div>
     </div>
+    <div :class="isOpen ? 'block' : 'hidden'" class="fixed w-full left-0 bg-gray-400 md:block md:static">
+        <ul class="md:flex md:justify-center md:items-center">
+            <li class="border-b">
+                <div class="my-4">
+                    <a href="{{ url('/farm/list') }}" class="block px-8 py-2 hover:bg-gray-600 rounded">ファーム</a>
+                </div>
+            </li>
+            <li class="border-b">
+                <div class="my-4">
+                    <a href="{{ url('/review/list') }}" class="block px-8 py-2 hover:bg-gray-600 rounded">レビュー</a>
+                </div>
+            </li>
+            <li class="border-b">
+                <div class="my-4">
+                    <a href="{{ url('/tip/list') }}" class="block px-8 py-2 hover:bg-gray-600 rounded">Tip</a>
+                </div>
+            </li>
+            <li class="border-b">
+                <div class="my-4">
+                    <a href="{{ url('/content/list') }}" class="block px-8 py-2 hover:bg-gray-600 rounded">コンテンツ</a>
+                </div>
+            </li>
+        </ul>
+    </div>
 </header>
+</div>
