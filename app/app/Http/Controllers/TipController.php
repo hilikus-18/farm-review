9d344ae5c8e68fd4822f9cfb1e39222151abf73b
Hiroki Senoo <hunuaaaa@gmail.com>
2022-02-15 01:37:11 +0900

    Tipsのリストページへの表示

--- app/app/Http/Controllers/TipController.php
+++ app/app/Http/Controllers/TipController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tip;
 use Illuminate\Http\Request;
 
 class TipController extends Controller
 {
     public function index() {
-        return view('Tip.list');
+        $tips = \App\Models\Tip::all();
+        return view('Tip.list', ['tips' => $tips]);
     }
 }
--- app/resources/views/Tip/list.blade.php
+++ app/resources/views/Tip/list.blade.php
@@ -11,15 +11,21 @@
                 <tbody>
                     <tr>
                         <th><label for="">収穫物から探す</label></th>
-                        <td><div></div></td>
+                        <td>
+                            <div></div>
+                        </td>
                     </tr>
                     <tr>
                         <th><label for="">作業種別から探す</label></th>
-                        <td><div></div></td>
+                        <td>
+                            <div></div>
+                        </td>
                     </tr>
                     <tr>
                         <th><label for="">フリーワード</label></th>
-                        <td><div></div></td>
+                        <td>
+                            <div></div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
@@ -31,5 +37,19 @@
     <div>
         <p>全<span></span>件中<span></span>件表示</p>
     </div>
+</div>
+<div>
+    @foreach ($tips as $tip)
+    <div>
+        <div>
+            <a href="{{ url('/tip/detail/'.$tip->id) }}">
+                <div>
+                    <div>{{ $tip->title }}</div>
+                    <div>{{ $tip->text }}</div>
+                </div>
+            </a>
+        </div>
+    </div>
+    @endforeach
 </div>
 @endsection
